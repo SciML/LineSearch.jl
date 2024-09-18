d74dc2060cd505397b9da23b9ad6928c40258fca
Avik Pal <avikpal@mit.edu>
2024-09-18 17:15:07 -0400

    feat: get started with the package code

--- src/LineSearch.jl
+++ src/LineSearch.jl
@@ -1,5 +1,26 @@
 module LineSearch
 
-# Write your package code here.
+using ADTypes: ADTypes
+using CommonSolve: CommonSolve
+using ConcreteStructs: @concrete
+using DifferentiationInterface: DifferentiationInterface
+using FastClosures: @closure
+
+const DI = DifferentiationInterface
+
+abstract type AbstractLineSearchAlgorithm end
+
+include("li_fukushima.jl")
+include("no_search.jl")
+include("robust_non_monotone.jl")
+
+include("line_searches_ext.jl")
+
+@concrete struct LineSearchSolution
+    alpha <: Real
+    success::Bool
+end
+
+export LineSearchSolution
 
 end
