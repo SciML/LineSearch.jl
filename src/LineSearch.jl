c7c11039e01867db6d143852a4f513236d0d6b1f
Avik Pal <avikpal@mit.edu>
2024-09-18 17:24:11 -0400

    feat: add NoLineSearch

--- src/LineSearch.jl
+++ src/LineSearch.jl
@@ -5,10 +5,12 @@ using CommonSolve: CommonSolve
 using ConcreteStructs: @concrete
 using DifferentiationInterface: DifferentiationInterface
 using FastClosures: @closure
+using SciMLBase: AbstractNonlinearProblem, ReturnCode
 
 const DI = DifferentiationInterface
 
 abstract type AbstractLineSearchAlgorithm end
+abstract type AbstractLineSearchCache end
 
 include("li_fukushima.jl")
 include("no_search.jl")
@@ -17,10 +19,12 @@ include("robust_non_monotone.jl")
 include("line_searches_ext.jl")
 
 @concrete struct LineSearchSolution
-    alpha <: Real
-    success::Bool
+    step_size <: Real
+    retcode::ReturnCode.T
 end
 
 export LineSearchSolution
+
+export NoLineSearch, NoLineSearchCache
 
 end
