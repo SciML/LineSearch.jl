7446cfd1f29daa24095facd00c14ca298a3b0ab7
Avik Pal <avikpal@mit.edu>
2024-09-26 04:57:39 -0400

    docs: add initial documentation

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using Documenter, DocumenterCitations
+using LineSearch
+
+cp(joinpath(@__DIR__, "Manifest.toml"),
+    joinpath(@__DIR__, "src/assets/Manifest.toml"), force = true)
+cp(joinpath(@__DIR__, "Project.toml"),
+    joinpath(@__DIR__, "src/assets/Project.toml"), force = true)
+
+include("pages.jl")
+
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"))
+
+makedocs(; sitename = "LineSearch.jl",
+    authors = "Avik Pal",
+    modules = [LineSearch],
+    clean = true,
+    doctest = false,
+    linkcheck = true,
+    checkdocs = :exports,
+    warnonly = [:missing_docs],
+    plugins = [bib],
+    format = Documenter.HTML(assets = ["assets/favicon.ico", "assets/citations.css"],
+        canonical = "https://docs.sciml.ai/LineSearch/stable/"),
+    pages)
+
+deploydocs(repo = "github.com/SciML/LineSearch.jl.git"; push_preview = true)
---
+++ docs/pages.jl
@@ -0,0 +1,12 @@
+# Put in a separate page so it can be used by SciMLDocs.jl
+
+pages = [
+    "index.md",
+    "API Reference" => [
+        "Native Functionalities" => "api/native.md",
+    ],
+    "Wrapped Algorithms API" => [
+        "LineSearches.jl" => "api/line_searches.md",
+    ],
+    "References" => "references.md"
+]
