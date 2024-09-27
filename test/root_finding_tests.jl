7feabfea682725e5b380c8eb51a2f0213d372fa0
Avik Pal <avikpal@mit.edu>
2024-09-27 17:20:33 -0400

    test: nonlinear root finding tests

---
+++ test/root_finding_tests.jl
@@ -0,0 +1,4 @@
+# Here we write out Newton Raphson and test integration with LineSearch.jl. Main tests are
+# over at NonlinearSolve.jl and SimpleNonlinearSolve.jl
+@testsetup module RootFinding
+end
