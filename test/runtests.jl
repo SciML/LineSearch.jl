b474e02376654ab81b3d6a08efbf7f968e8f33e4
Avik Pal <avikpal@mit.edu>
2024-09-26 21:33:52 -0400

    test: custom optimizer tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
-using TestItemRunner, InteractiveUtils
+using ReTestItems, LineSearch, Hwloc, InteractiveUtils
 
 @info sprint(InteractiveUtils.versioninfo)
 
-@run_package_tests
+const GROUP = lowercase(get(ENV, "GROUP", "All"))
+
+const RETESTITEMS_NWORKERS = parse(
+    Int, get(ENV, "RETESTITEMS_NWORKERS", string(min(Hwloc.num_physical_cores(), 4))))
+const RETESTITEMS_NWORKER_THREADS = parse(Int,
+    get(ENV, "RETESTITEMS_NWORKER_THREADS",
+        string(max(Hwloc.num_virtual_cores() ÷ RETESTITEMS_NWORKERS, 1))))
+
+@info "Running tests for group: $(GROUP) with $(RETESTITEMS_NWORKERS) workers"
+
+ReTestItems.runtests(LineSearch; tags = (GROUP == "all" ? nothing : [Symbol(GROUP)]),
+    nworkers = RETESTITEMS_NWORKERS, nworker_threads = RETESTITEMS_NWORKER_THREADS,
+    testitem_timeout = 3600)
