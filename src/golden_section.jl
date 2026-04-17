75c7483203e6c43c3d9466518d1325da1a7add67
Iskaban10 <janasourish197@gmail.com>
2026-04-18 01:16:13 +0530

    shifted phi and resphi to init from solve

--- src/golden_section.jl
+++ src/golden_section.jl
@@ -16,6 +16,8 @@ end
     u_cache
     fu_cache
     α
+    φ
+    resphi
     stats <: Union{SciMLBase.NLStats, Nothing}
     alg <: GoldenSection
     maxiters::Int
@@ -26,6 +28,9 @@ function CommonSolve.init(
         stats::Union{SciMLBase.NLStats, Nothing} = nothing, kwargs...
     )
     T = promote_type(eltype(fu), eltype(u))
+
+    φ = (sqrt(T(5)) + 1) / 2
+    resphi = 2 - φ 
 
     @bb u_cache = similar(u)
     @bb fu_cache = similar(fu)
@@ -38,7 +43,7 @@ function CommonSolve.init(
     end
 
     return GoldenSectionCache(
-        ϕ, prob.f, prob.p, u_cache, fu_cache, T(1), stats, alg, alg.maxiters
+        ϕ, prob.f, prob.p, u_cache, fu_cache, T(1), φ, resphi, stats, alg, alg.maxiters
     )
 end
 
@@ -46,22 +51,20 @@ function CommonSolve.solve!(cache::GoldenSectionCache, u, du)
     T = promote_type(eltype(du), eltype(u))
     ϕ = @closure α -> cache.ϕ(cache.f, cache.p, u, du, α, cache.u_cache, cache.fu_cache)
 
-    a, b = zero(T), T(cache.α)
-    φ = (sqrt(T(5)) + 1) / 2
-    resphi = 2 - φ  # ≈ 0.382
+    a, b = zero(T), T(cache.α) 
 
-    x1 = a + resphi * (b - a)
-    x2 = b - resphi * (b - a)
+    x1 = a + cache.resphi * (b - a)
+    x2 = b - cache.resphi * (b - a)
     f1, f2 = ϕ(x1), ϕ(x2)
 
     for _ in 1:(cache.maxiters)
         abs(b - a) ≤ cache.alg.tol && break
         if f1 < f2
             b = x2;  x2 = x1;  f2 = f1
-            x1 = a + resphi * (b - a);  f1 = ϕ(x1)
+            x1 = a + cache.resphi * (b - a);  f1 = ϕ(x1)
         else
             a = x1;  x1 = x2;  f1 = f2
-            x2 = b - resphi * (b - a);  f2 = ϕ(x2)
+            x2 = b - cache.resphi * (b - a);  f2 = ϕ(x2)
         end
     end
 
